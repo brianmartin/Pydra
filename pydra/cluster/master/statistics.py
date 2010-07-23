161d409a3db000a67c414f655cf835e7804cbb7b
Brian Martin <brianmartin@gmail.com>
2010-07-23 13:45:27 -0400

    whitespace fixes (tabs for spaces)

--- pydra/cluster/master/statistics.py
+++ pydra/cluster/master/statistics.py
@@ -33,40 +33,40 @@ logger = logging.getLogger('root')
 class StatisticsModule(Module):
 
     def __init__(self):
-	self._friends = {
-	    'scheduler' : TaskScheduler,
-	}
+        self._friends = {
+            'scheduler' : TaskScheduler,
+        }
 
-	self._interfaces = [
-	    self.stats,
-	]
+        self._interfaces = [
+            self.stats,
+        ]
 
     def _register(self, manager):
         Module._register(self, manager)
-	reactor.callLater(2, self.stats)
+        reactor.callLater(2, self.stats)
 
     def stats(self, callback=None):
-	tasks = TaskInstance.objects.all()
-	sums = {}
-	for task in tasks: 
-	    try:
-		sums[task.task_key]
-	    except KeyError:
-		sums[task.task_key] = {'num_completed': 0, 'summed_time': 0, \
-				       'summed_squared_time': 0}
-	    if task.completed:
-		time_delta = (task.completed - task.started).seconds
-		sums[task.task_key]['summed_time'] += time_delta
-		sums[task.task_key]['summed_squared_time'] += time_delta**2
-		sums[task.task_key]['num_completed'] += 1
+        tasks = TaskInstance.objects.all()
+        sums = {}
+        for task in tasks: 
+            try:
+                sums[task.task_key]
+            except KeyError:
+                sums[task.task_key] = {'num_completed': 0, 'summed_time': 0, \
+                                       'summed_squared_time': 0}
+            if task.completed:
+                time_delta = (task.completed - task.started).seconds
+                sums[task.task_key]['summed_time'] += time_delta
+                sums[task.task_key]['summed_squared_time'] += time_delta**2
+                sums[task.task_key]['num_completed'] += 1
 
-	# TODO: optimize by switching to a running variance/stddev calculation
-	for task, values in sums.items():
-	    sums[task]['std_dev'] = sqrt((sums[task]['summed_squared_time'] - (sums[task]['summed_time']**2 \
-					/ sums[task]['num_completed'])) / (sums[task]['num_completed'] - 1))	
+        # TODO: optimize by switching to a running variance/stddev calculation
+        for task, values in sums.items():
+            sums[task]['std_dev'] = sqrt((sums[task]['summed_squared_time'] - (sums[task]['summed_time']**2 \
+                                        / sums[task]['num_completed'])) / (sums[task]['num_completed'] - 1))    
 
-	logger.info("task stats: %s" % str(sums))
-	self.recall()
+        logger.info("task stats: %s" % str(sums))
+        self.recall()
 
     def recall(self):
-	reactor.callLater(2, self.stats)
+        reactor.callLater(2, self.stats)
