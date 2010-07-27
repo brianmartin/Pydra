1a17ae01a345e2046316d43d5ed6e98ce356ef30
Brian Martin <brianmartin@gmail.com>
2010-07-27 14:17:34 -0400

    statistics.py: Recalculate stats based on task_key.

--- pydra/cluster/master/statistics.py
+++ pydra/cluster/master/statistics.py
@@ -20,11 +20,10 @@ from twisted.internet import reactor
 
 from datetime import datetime
 from math import sqrt
-from pydra.cluster.module import Module
-from pydra.cluster.master.scheduler import TaskScheduler
-import pydra_settings
 
-from pydra.models import TaskInstance
+from pydra.cluster.module import Module
+from pydra.models import TaskInstance, WorkUnit
+import pydra_settings
 
 # init logging
 import logging
@@ -33,51 +32,77 @@ logger = logging.getLogger('root')
 class StatisticsModule(Module):
 
     def __init__(self):
-        self._friends = {
-            'scheduler' : TaskScheduler,
-        }
-
         self._interfaces = [
+            self.calc,
             self.stats,
         ]
 
         self.sums = {}
-        self.index = 0
+        self.indices = {}
 
     def _register(self, manager):
         Module._register(self, manager)
-        reactor.callLater(2, self.stats)
+        reactor.callLater(2, self.calc)
 
-    def stats(self, callback=None):
-        tasks = TaskInstance.objects.exclude(completed=None)[self.index:]
-        self.index += tasks.count()
-        for task in tasks: 
+    def calc(self):
+        """
+        Recalculates stats for all unique tasks that have been run.
+        """
+        for task_key in set(TaskInstance.objects.values_list('task_key')): 
+            results = self.stats(task_key[0])
+            logger.info("%s stats: %s" % (task_key[0], results)
+        reactor.callLater(2, self.calc)
+
+    def stats(self, task_key):
+        """
+        Returns task statistics given the task key.
+        """
+
+        try: 
+            self.indices[task_key]
+        except KeyError:
+            self.indices[task_key] = 0
+
+        tasks = TaskInstance.objects.filter(task_key=task_key).exclude(completed=None)[self.indices[task_key]:]
+        #if the task has been run since the last update, do:
+        if tasks:
+            self.indices[task_key] += tasks.count()
+            for task in tasks: 
+                try:
+                    self.sums[task_key]
+                except KeyError:
+                    self.sums[task_key] = {'num_completed': 0, 'summed_time': 0, \
+                            'summed_squared_time': 0, 'min': -1, 'max': -1, 'std_dev': -1}
+                time_delta = (task.completed - task.started).seconds
+
+                # max
+                if time_delta > self.sums[task_key]['max']:
+                    self.sums[task_key]['max'] = time_delta
+
+                # min
+                if time_delta < self.sums[task_key]['min'] or self.sums[task_key]['min'] == -1:
+                    self.sums[task_key]['min'] = time_delta
+
+                # sums
+                self.sums[task_key]['summed_time'] += time_delta
+                self.sums[task_key]['summed_squared_time'] += time_delta**2
+                self.sums[task_key]['num_completed'] += 1
+
+            # standard deviation
+            # TODO: optimize by switching to a running stddev calculation
+            if self.sums[task_key]['num_completed'] - 1:
+                self.sums[task_key]['std_dev'] = sqrt((self.sums[task_key]['summed_squared_time'] - (self.sums[task_key]['summed_time']**2 \
+                                                   / self.sums[task_key]['num_completed'])) / (self.sums[task_key]['num_completed'] - 1))    
+            return self.sums[task_key]
+
+        else:
             try:
-                self.sums[task.task_key]
+                # if previously calculated stats exist return them
+                return self.sums[task_key]
             except KeyError:
-                self.sums[task.task_key] = {'num_completed': 0, 'summed_time': 0, \
-                                            'summed_squared_time': 0, 'min': -1, 'max': -1}
-            time_delta = (task.completed - task.started).seconds
+                logger.info("%s has not yet been run or is not yet completed." % task_key)
+                return {}
 
-            # max
-            if time_delta > self.sums[task.task_key]['max']:
-                self.sums[task.task_key]['max'] = time_delta
-
-            # min
-            if time_delta < self.sums[task.task_key]['min'] or self.sums[task.task_key]['min'] == -1:
-                self.sums[task.task_key]['min'] = time_delta
-
-            # sums
-            self.sums[task.task_key]['summed_time'] += time_delta
-            self.sums[task.task_key]['summed_squared_time'] += time_delta**2
-            self.sums[task.task_key]['num_completed'] += 1
-
-        # standard deviation
-        # TODO: optimize by switching to a running variance/stddev calculation
-        for task, values in self.sums.items():
-            if self.sums[task]['num_completed'] - 1:
-                self.sums[task]['std_dev'] = sqrt((self.sums[task]['summed_squared_time'] - (self.sums[task]['summed_time']**2 \
-                                            / self.sums[task]['num_completed'])) / (self.sums[task]['num_completed'] - 1))    
-
-        logger.info("task stats: %s" % str(self.sums))
-        reactor.callLater(2, self.stats)
+    def list_workunits(self):
+        for task in TaskInstance.objects.all():
+            logger.info("%s %s" (task.task_key, str(WorkUnit.objects.filter(task_instance=task))))
